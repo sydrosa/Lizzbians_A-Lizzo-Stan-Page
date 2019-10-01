72ee4d1238ea8fa9483cff534aab6b43eb612d91
andrewrosario <rosario.andrew@gmail.com>
2019-09-30 22:10:12 -0500

    leaderboard functional

--- lizzbians-front-end/app/main.js
+++ lizzbians-front-end/app/main.js
@@ -1,3 +1,9 @@
+// Remove Content From Inner-Conent Div
+function clearInnerContent(innerContentWrapper) {
+    innerContentWrapper.innerHTML = ''
+}
+
+// Build Theme Menu
 function renderThemeSettings(wrapper) {
     const sidebarWrapper = document.getElementById('sidebar-wrapper')
 
@@ -5,16 +11,14 @@ function renderThemeSettings(wrapper) {
     classicButton.innerText = 'Classic'
     wrapper.appendChild(classicButton)
     classicButton.addEventListener('click', function(event) {
-        // sidebarWrapper.classList.remove('light-theme');
-        // sidebarWrapper.classList.remove('dark-theme');
+        event.preventDefault()
         sidebarWrapper.setAttribute('class', 'classic-theme')
     })
 
     const darkButton = document.createElement('button')
     darkButton.innerText = 'Dark'
     darkButton.addEventListener('click', function(event) {
-        // sidebarWrapper.classList.remove('classic-theme');
-        // sidebarWrapper.classList.remove('dark-theme');
+        event.preventDefault()
         sidebarWrapper.setAttribute('class', 'dark-theme')
     })
     wrapper.appendChild(darkButton)
@@ -22,33 +26,87 @@ function renderThemeSettings(wrapper) {
     const lightButton = document.createElement('button')
     lightButton.innerText = 'Light'
     lightButton.addEventListener('click', function(event) {
-        // sidebarWrapper.classList.remove('classic-theme');
-        // sidebarWrapper.classList.remove('dark-theme');
+        event.preventDefault()
         sidebarWrapper.setAttribute('class', 'light-theme')
     })
     wrapper.appendChild(lightButton)   
 }
 
+// Leaderboard Table Helper Methods
+function renderLeaderTables(gameTypeDiv, type) {
+    const scoresTitle = document.createElement('h2')
+    scoresTitle.innerText = `${type} Scores Table`
+    gameTypeDiv.appendChild(scoresTitle)
 
+    const scoresTable = document.createElement('table')
+    scoresTable.setAttribute('id', type)
+    gameTypeDiv.appendChild(scoresTable)
 
+    const scoresHeaderRow = document.createElement('tr')
+    scoresTable.appendChild(scoresHeaderRow)
+
+    const scoresNameHeader = document.createElement('th')
+    scoresNameHeader.innerText = 'Name'
+    scoresHeaderRow.appendChild(scoresNameHeader)
+
+    const scoreHeader = document.createElement('th')
+    scoreHeader.innerText = 'Score'
+    scoresHeaderRow.appendChild(scoreHeader)
+}
+
+// Build Leaderboard
+function renderLeaderboard(wrapper, type) {
+    fetch(`http://localhost:3000/games/${type}`)
+    .then(resp => resp.json())
+    .then(resp => {
+        console.log(resp)
+        const scoresDiv = document.createElement('div')
+        wrapper.appendChild(scoresDiv)
+        renderLeaderTables(scoresDiv, type)
+        const thisTable = document.getElementById(`${type}`)
+        if(resp.length === 0) {
+            const noScores = document.createElement('p')
+            noScores.innerText = 'There are no Scores yet. Play?'
+            scoresDiv.appendChild(noScores)
+        } else {
+            for(let i = 0; i < resp.length; i++) {
+                const thisRow = document.createElement('tr')
+                thisTable.appendChild(thisRow)
+                
+                const myName = document.createElement('td')
+                myName.innerText = resp[i].user.username
+                thisRow.appendChild(myName)
+
+                const myScore = document.createElement('td')
+                myScore.innerText = resp[i].score
+                thisRow.appendChild(myScore)
+            }
+        }
+    })
+}
+
+// On Page Load Functions
 $(document).ready(function(){
     const themeButton = document.getElementById('theme')
+    const leaderboardButton = document.getElementById('leaderboard')
     const innerContentWrapper = document.getElementById('inner-content')
     
-    $("#menu-toggle").click(function(e){
-        e.preventDefault();
-        $("#wrapper").toggleClass("menuDisplayed");
+    $("#menu-toggle").click(function(event){
+        event.preventDefault()
+        $("#wrapper").toggleClass("menuDisplayed")
     });
 
     themeButton.addEventListener('click', (event) => {
+        clearInnerContent(innerContentWrapper)
         renderThemeSettings(innerContentWrapper)
     })
 
-
-
-
-
-
+    leaderboardButton.addEventListener('click', (event) => {
+        console.log('click')
+        clearInnerContent(innerContentWrapper)
+        renderLeaderboard(innerContentWrapper, 'speed')
+        renderLeaderboard(innerContentWrapper, 'regular')
+    })
 
 
 
