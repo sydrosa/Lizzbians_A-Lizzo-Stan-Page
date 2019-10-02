394a67f27f91da29170d3029dde1bb912bd9ba3b
andrewrosario <rosario.andrew@gmail.com>
2019-10-02 11:28:06 -0500

    game page loading, answer event listeners are not.

---
+++ lizzbians-back-end/app/controllers/authentication_controller.rb
@@ -0,0 +1,39 @@
+class JsonWebToken
+    SECRET_KEY = Rails.application.secrets.secret_key_base. to_s
+  
+    def self.encode(payload, exp = 24.hours.from_now)
+      payload[:exp] = exp.to_i
+      JWT.encode(payload, SECRET_KEY)
+    end
+  
+    def self.decode(token)
+      decoded = JWT.decode(token, SECRET_KEY)[0]
+      HashWithIndifferentAccess.new decoded
+    end
+  end
+
+class AuthenticationController < ApplicationController
+    before_action :authorize_request, except: :login
+
+
+
+    # POST /auth/login
+    def login
+      @user = User.find_by(username: params[:username])
+    #   byebug
+      if @user
+        token = JsonWebToken.encode(user_id: @user.id)
+        time = Time.now + 24.hours.to_i
+        render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
+                       username: @user.username }, status: :ok
+      else
+        render json: { error: 'unauthorized' }, status: :unauthorized
+      end
+    end
+  
+    private
+  
+    def login_params
+      params.permit(:email, :password)
+    end
+end
--- lizzbians-front-end/app/reg.js
+++ lizzbians-front-end/app/reg.js
@@ -1,35 +1,67 @@
+var shuffle = function (array) {
+
+	var currentIndex = array.length;
+	var temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+
+};
+
 document.addEventListener('DOMContentLoaded', (event) => {
+    const testButton = document.getElementById('game-test')
+    let questions
 
-let questions
-
-function getQuestions() {
     const questionsURL = 'http://localhost:3000/questions'
+
     fetch(questionsURL)
     .then(resp => resp.json())
     .then(resp => {
-        questions = resp
+        questions = shuffle(resp)
+        
     })
-}
+    
+    function displayQuestion() {
+        const gameDiv = document.getElementById('game-div')
+        const questionContent = document.getElementById('question-content')
+        const answerContentDivs = document.getElementsByClassName('answer-content')
+        const thisQuestion = questions[questions.length -1]
+        gameDiv.classList.remove('hidden')
+    
+        questionContent.innerText = thisQuestion.content;
 
-function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+        for(let i = 0; i < thisQuestion.answers.length; i++) {
+            const myAnswers = thisQuestion.answers
+            console.log(myAnswers)
+            const thisDiv = answerContentDivs[i].parentNode
+            answerContentDivs[i].innerText = myAnswers[i].content
+            console.log(thisQuestion.answers[i])
+            thisDiv.setAttribute('id', myAnswers[i].id)
+
+            thisDiv.addEventListener('click', (event) => {
+                
+                console.log(this)
+                fetch(`http://localhost:3000/answers/${event.target.id}`)
+                .then(resp => resp.json())
+                .then(resp => console.log(resp))
+            })
+        }
     }
-    return array;
-}
 
-function displayQuestion() {
-    const gameDiv = document.getElementById('game-div')
-    const questionContent = document.getElementById('question-content')
-    const answerContentPs = document.getElementsByClassName('answer-content')
-    gameDiv.classList.remove('hidden')
-
-    questionContent.innerText = questions[questions.length -1].content
-
-
-
-}
+    testButton.addEventListener('click', (event) => {
+        displayQuestion()
+    })
 
 
 
@@ -38,10 +70,4 @@ function displayQuestion() {
 
 
 
-
-
-
-
-
-
-});
+})
