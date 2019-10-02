be3099481ba45b3f8781d9508924acc68fae788e
Syd <green@Syds-MacBook-Air-2.local>
2019-10-02 14:18:08 -0500

    adding countdown timer

--- lizzbians-front-end/app/reg.js
+++ lizzbians-front-end/app/reg.js
@@ -44,12 +44,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
         for(let i = 0; i < thisQuestion.answers.length; i++) {
             const myAnswers = thisQuestion.answers
             console.log(myAnswers)
-            const thisButton = answerContentButtons[i]
-            answerContentButtons[i].innerText = myAnswers[i].content
+            const thisDiv = answerContentDivs[i].parentNode
+            answerContentDivs[i].innerText = myAnswers[i].content
             console.log(thisQuestion.answers[i])
-            thisButton.setAttribute('id', myAnswers[i].id)
+            thisDiv.setAttribute('id', myAnswers[i].id)
 
-            thisButton.addEventListener('click', (event) => {
+            thisDiv.addEventListener('click', (event) => {
+                
+                console.log(this)
                 fetch(`http://localhost:3000/answers/${event.target.id}`)
                 .then(resp => resp.json())
                 .then(resp => console.log(resp))
@@ -61,5 +63,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
         displayQuestion()
     })
 
+    function countDownTimer() {
+        const timer = document.getElementById('time-goes-here')
+        var i = 10;
+        var ticker = setInterval(function () {
+            timer.innerHTML = i;
+            if (i === 0) {
+                clearInterval(ticker);
+            }
+            else   {
+                    i--;
+                    }
+                }, 1000);
+    };
 
+    countDownTimer();
 })
